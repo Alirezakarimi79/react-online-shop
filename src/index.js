1a51133db27f6c637217e4e0cd500b9d2cca222a
Alireza Karimi <79Alirezakarimi79@gamil.com>
2022-04-18 19:35:31 +0430

    feat: create-navbar component in create-navbar branch

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+
+import App from "./containers/App";
+
+render(<App />, document.getElementById("root"));
